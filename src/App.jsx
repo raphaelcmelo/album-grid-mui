b3ed4aee2ee83d3b579632c57bf4fd3ce0dea53b
Raphael Cassimiro <raphaelcmelob.b@gmail.com>
2023-05-17 15:06:47 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
+
+import useStyles from './styles';
+
+const App = () => {
+  const classes = useStyles();
+
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  return (
+    <>
+    <CssBaseline />
+    <AppBar position='relative'>
+      <Toolbar>
+        <PhotoCamera className={classes.icon}/>
+        <Typography variant='h6'>
+          Photo Album
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <main>
+      <div className={classes.container}>
+        <Container maxWidth='sm'>
+          <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+            Photo Album
+          </Typography>
+          <Typography variant='h5' align='center' color='textSecondary' paragraph>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel labore repellat totam, reiciendis similique laudantium distinctio accusantium mollitia, culpa at doloribus, ipsa odio repellendus id optio facilis veniam nostrum voluptatibus!
+          </Typography>
+          <div className={classes.container}>
+            <Grid container spacing={2} justifyContent='center'>
+              <Grid item>
+                <Button variant='contained' color='primary'>
+                  Aperte aqui
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant='outlined' color='primary'>
+                  Aperte aqui
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+          <Container className={classes.cardGrid} maxWidth='md'>
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia 
+                    className={classes.cardMedia}
+                    image='https://source.unsplash.com/random'
+                    title='Image title'
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant='h5'>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions className={classes.cardActions}>
+                    <Button variant='contained' size='small' color='primary'>View</Button>
+                    <Button variant='contained' size='small' color='primary'>Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </Container>
+      </div>
+    </main>
+    <footer className='classes.footer'>
+      <Typography variant='h6' align='center' gutterBottom>
+        Footer
+      </Typography>
+      <Typography variant='subtitle1' align='center' color='textSecondary'>
+        Something here to give the footer a purpose!
+      </Typography>
+    </footer>
+    </>
+  )
+}
+
+export default App;
